e9cbd18422cc44396853774b21e18db60cbabeaa
gustavotondincei <gus.tondin@alumnos.cei.es>
2026-03-03 11:56:08 +0100

    Class 25
    JS Menu

---
+++ js/index.js
@@ -0,0 +1,8 @@
+
+let navMenu = document.getElementById('nav-menu')
+let menuBtn = document.getElementById('menu-btn')
+
+function toggleMenu() {
+    navMenu.classList.toggle('activeMenu')
+    menuBtn.classList.toggle('activeBtn')
+}
